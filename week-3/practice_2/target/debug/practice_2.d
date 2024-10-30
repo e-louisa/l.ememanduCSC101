d9eceaf46bda7124b4ca0e46a68797c9c81e9691
e-louisa <louisa.ememandu@pau.edu.ng>
2024-10-30 16:55:25 +0100

    (my wk3 commit)

---
+++ week-3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\louis\OneDrive\Documents\l.ememanduCSC101\week-3\practice_2\target\debug\practice_2.exe: C:\Users\louis\OneDrive\Documents\l.ememanduCSC101\week-3\practice_2\src\main.rs
---
+++ week-3/practice_7/target/debug/deps/practice_7.d
@@ -0,0 +1,5 @@
+C:\Users\louis\OneDrive\Documents\l.ememanduCSC101\week-3\practice_7\target\debug\deps\practice_7.exe: src/main.rs
+
+C:\Users\louis\OneDrive\Documents\l.ememanduCSC101\week-3\practice_7\target\debug\deps\practice_7.d: src/main.rs
+
+src/main.rs:
